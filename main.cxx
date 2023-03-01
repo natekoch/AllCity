19dc76b2ec96c940531ded4cea9b22e15a81995b
Nate Koch <31897172+natekoch@users.noreply.github.com>
2023-03-01 10:46:47 -0800

    initial commit of main.cxx

---
+++ main.cxx
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <unistd.h>
+
+using std::endl;
+using std::cerr;
+
+#include <GLFW/glfw3.h>
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtx/string_cast.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp> 
+
+class RenderManager;
+
+int main()
+{
+  return 0;
+}
